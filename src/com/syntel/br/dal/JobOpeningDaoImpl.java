553d4ebffc822b0c2df675df3ab715fb6c669e35
codeboyatwork <tanmaydeshmukh1@gmail.com>
2017-11-05 21:36:46 +0530

    Implemented Methods in the JobOpeningDaoImpl

--- src/com/syntel/br/dal/JobOpeningDaoImpl.java
+++ src/com/syntel/br/dal/JobOpeningDaoImpl.java
@@ -2,6 +2,7 @@ package com.syntel.br.dal;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -41,14 +42,59 @@ public class JobOpeningDaoImpl implements JobOpeningDao {
 
 	@Override
 	public boolean removeJobOpening(int jobCode) {
-		// TODO Auto-generated method stub
-		return false;
+		Connection con=null;
+		PreparedStatement ps =null;
+		boolean isJobOpeningDeleted = false;
+		System.out.println("Logger >>> inside removeJobOpening method @ JobOpeningDaoImpl");
+		try {
+			
+			con=DBUtil.getConnection();
+			ps=con.prepareStatement("Delete from JobOpening where jobCode=?");
+			ps.setInt(1, jobCode);
+			int del = ps.executeUpdate();
+			if(del == 1)
+				isJobOpeningDeleted = true;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally
+		{
+			DBUtil.close(con);
+		}
+	    return isJobOpeningDeleted;
 	}
 
 	@Override
 	public boolean updateJobOpening(JobOpening jo) {
-		// TODO Auto-generated method stub
-		return false;
+		Connection con=null;
+		PreparedStatement ps =null;
+		ResultSet rs = null;
+//		JobApplication jobApplication = null;
+		boolean isJobOpeningUpdated = false;
+
+		System.out.println("Logger >>> inside updateJobOpening Method @ JobOpeningDao");
+		try {
+			con=DBUtil.getConnection();
+			ps=con.prepareStatement("Update jobappplication SET  skills= ?,experience=?,jobLocation=?,spocName=?,emailID=?,rewards=? where jobCode=?");
+			ps.setString(1, jo.getSkills());
+			ps.setInt(2, jo.getExperience());
+			ps.setString(3, jo.getJobLocation());
+			ps.setString(4, jo.getSpocName());
+			ps.setString(5, jo.getEmailID());
+			ps.setInt(6, jo.getRewards());
+			
+			ps.setInt(7, jo.getJobCode());
+			int editStatusResult = ps.executeUpdate();
+			if(editStatusResult == 1)
+				isJobOpeningUpdated = true;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally
+		{
+			DBUtil.close(con);
+		}
+	    return isJobOpeningUpdated;
 	}
 
 	@Override
