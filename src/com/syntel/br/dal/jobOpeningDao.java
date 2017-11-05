609ef1dec366f2e43ceb439e724158bace15c657
Tanmay Deshmukh <tanmaydeshmukh1@gmail.com>
2017-11-05 11:28:24 +0530

    Changed a Name of the parameter

--- src/com/syntel/br/dal/jobOpeningDao.java
+++ src/com/syntel/br/dal/jobOpeningDao.java
@@ -9,7 +9,7 @@ public interface jobOpeningDao {
 	boolean addJobOpening(JobOpening jo);//Adds a new job opening 
 	boolean removeJobOpening(int jobCode);//Remove a job opening specified by the jobCode
 	boolean updateJobOpening(JobOpening jo);//Updates a job opening 
-	List<JobOpening> showJobOpenings(String isActiveStatus);//Returns a List of All available Job Openings based on the parameter passed
+	List<JobOpening> showJobOpenings(String isStatusFinal);//Returns a List of All available Job Openings based on the parameter passed
 	//apply();
 	Double calculateReward(int jobCode);//to get the rewards from db where input is jobcode
 	
